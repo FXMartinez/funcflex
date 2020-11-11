77c4b9fc1831bfaee4ab9df722ee2bafc335d2b5
FXMartinez <felixaviermartinez@gmail.com>
2020-11-11 13:12:33 -0500

    fixed issues with removeTags function returning undefined when used in article component

--- src/components/article.js
+++ src/components/article.js
@@ -5,16 +5,23 @@ import { Component } from "react";
 // import { Component } from "react";
 
 class Article extends Component {
+
+    state = {
+        content: ''
+    }
+
+    componentDidMount(){
+        this.setState({
+            content: this.props.post.content
+        })
+    }
       
     render() {
 
-        const content = this.props.post.content
-
         return ( 
-            // { content = this.props.post.content },
             
             <div className='wp-post' key={this.props.post.ID}>
-                    <h1> {this.props.post.title} </h1>
+                    <h1> { this.props.post.title } </h1>
 
                     <p>
                         {/* {props.post} */}
