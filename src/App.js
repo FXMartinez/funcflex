c282305feba24e336da3acc3c212ca1456ba7331
FXMartinez <felixaviermartinez@gmail.com>
2020-12-07 23:33:53 -0500

    added buttons for other blogs gaming, fitness, and buttons for social media

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Home from './containers/home';
 import NavBar from './components/nav_bar';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <NavBar />
-        <Home />
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    page: ''
+  }
+
+  getCurrentPage = (currentURL) => {
+    this.setState({
+        page: currentURL
+    })
+  }
+
+  render() {
+
+      return (
+        <div className="App">
+        <Router>
+          <NavBar getPage={this.getCurrentPage}/>
+          <Home />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,26 +1,79 @@
 import { Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
-function NavBar() {
+function NavBar(props) {
 
     return ( 
+
+        console.log(props),
+
         <div className='nav'>
-            <p>
+            <div className="func-flex">
+                <h1>
+                    <span style={{color: "Navy"}}>func </span>
+                    <span style={{color: "Khaki"}}>Flex</span>
+                    <span style={{color: "Gold"}}>(</span>
+                    <span style={{color: "SteelBlue"}}>{props.page || ""}</span>
+                    <span style={{color: "Gold"}}>)</span>
+                </h1>
+                {/* maybe turn this into a component that takes in other operations */}
+            </div>
+
+            <div>
                 
                 <Button 
                     color='black' content='Home' 
-                    icon={{ color: 'blue', name: 'like' }}
+                    icon={{ color: 'blue', name: 'terminal' }}
                     as={ Link }
                     to='/'
                     />
                 <Button 
                     color='black' content='Coding' 
-                    icon={{ color: 'red', name: 'like' }}
+                    icon={{ color: 'red', name: 'terminal' }}
                     as={ Link }
                     to='/Blog'
-                />
+                    />
+                <Button
+                    color='black' content='Gaming'
+                    icon={{ color: 'green', name: 'terminal'}}
+                    as={ Link }
+                    to='/Gaming' 
+                    />
+                <Button
+                    color='black' content='Fitness'
+                    icon={{ color: 'yellow', name: 'terminal'}}
+                    as={ Link }
+                    to='/Fitness'
+                    />
 
-            </p>
+                <Button
+                    color='black'
+                    icon={{ color: 'red', name: 'youtube'}}
+                    // as={ Link }
+                    href='https://www.youtube.com/channel/UCzzUwbAKCCoplHIgvyqZ-fA'
+                    />
+                <Button 
+                    color='black'
+                    icon={{ color: 'blue', name: 'twitter'}}
+                    href='http://www.twitter.com/funcmasterflex'
+                    />
+                <Button
+                    color='black'
+                    icon={{ color: 'purple', name: 'twitch'}}
+                    href='http://www.twitch.com/funcmasterflex'
+                    />
+                <Button
+                    color='black'
+                    icon={{ color: 'blue', name: 'linkedin'}}
+                    href='https://www.linkedin.com/in/felix-xavier-martinez-77491429/'
+                    />
+                <Button
+                    color='black'
+                    icon={{ color: 'pink', name: 'instagram'}}
+                    href='https://www.instagram.com/flexingtonsteel'
+                    />
+            </div>
+
         </div>
 
     );
